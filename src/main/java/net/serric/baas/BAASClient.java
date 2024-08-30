5b49e35b3700326cbaa469e6e96d8caac4fd867d
SeerSerric <dillonqgardner@gmail.com>
2024-08-29 21:26:46 -0700

    Initial commit

---
+++ src/main/java/net/serric/baas/BAASClient.java
@@ -0,0 +1,10 @@
+package net.serric.baas;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BAASClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
